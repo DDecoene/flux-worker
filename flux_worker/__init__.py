8d7fd2025e289bb86ee29fd40e2ef55c6912a760
Dennis Decoene <DDecoene@users.noreply.github.com>
2026-03-29 16:08:50 +0200

    feat: export GenerateResult and exception types from package

--- flux_worker/__init__.py
+++ flux_worker/__init__.py
@@ -1,3 +1,5 @@
 from flux_worker.orchestrator import generate
+from flux_worker.result import GenerateResult
+from flux_worker.exceptions import UserError, VastAIError, FluxError
 
-__all__ = ["generate"]
+__all__ = ["generate", "GenerateResult", "UserError", "VastAIError", "FluxError"]
